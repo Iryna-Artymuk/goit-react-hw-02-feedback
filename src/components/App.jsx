9c7495860b8d433dfdc5ff84668d6142a3ee4aaa
Iryna Artymuk <artimyk@ukr.net>
2023-03-03 23:41:49 +0200

    task1
    make feedback  rating form +css +js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Feedback from '../components/Feedback/Feedback';
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  addPoint = value => {
+    const obj = {};
+
+    this.setState(prevState => {
+      for (const key of Object.keys(prevState)) {
+        obj[key] = key === value ? prevState[key] + 1 : prevState[key];
+      }
+
+      return obj;
+    });
+  };
+  render() {
+    return (
+      <Feedback
+        addPoint={this.addPoint}
+        good={this.state.good}
+        neutral={this.state.neutral}
+        bad={this.state.bad}
+      />
+    );
+  }
+}
+export default App;
